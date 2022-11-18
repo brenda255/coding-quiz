c439d592228a597f5d245407b590292def1f53ad
brenda <115198624+brenda255@users.noreply.github.com>
2022-11-17 22:19:44 -0700

    updated README and js file

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # coding-quiz
+##Description
+In this project I created a coding quiz with a timer. The user has a certain amount of time to get the quiz completed. If they get an incorrect answer, then they are penalized with time off the clock. The quiz ends as soon as the user answers all the questions or until they run out of time. When they are all done, they get to input their initials and see who has the top highscores. 
+
+URL Link: 
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,8 @@
 
-
 var timerEl = document.querySelector(".timer");
 var countdownEl = document.getElementById("countdown");
-var startingMinutes =2;
-var secondsLeft = startingMinutes *60;
+var startingMinutes = 2;
+var secondsLeft = startingMinutes * 60;
 var button = document.querySelector(".button");
 var question = document.querySelector(".question");
 var questionsEl = document.querySelector("#quiz-questions");
@@ -17,25 +16,25 @@ var highscoresEl = document.querySelector(".highscores")
 var remainingTime;
 var quizQuestionsEl = [
     {
-        question:"Which is a string?", 
+        question: "Which is a string?",
         answerOne: "true",
         answerTwo: "22",
         answerThree: "quotation marks",
         correctAnswer: "quotation marks"
-    },{
-        question:"What is CSS used for?", 
+    }, {
+        question: "What is CSS used for?",
         answerOne: "display a function",
         answerTwo: "for style",
         answerThree: "math equation",
         correctAnswer: "for style",
-    },{
-        question:"What direction is JavaScript read?", 
+    }, {
+        question: "What direction is JavaScript read?",
         answerOne: "top to bottom",
         answerTwo: "any direction",
         answerThree: "bottom to top",
         correctAnswer: "top to bottom",
-    },{
-        question:"Do you link your stylesheet in the html page?", 
+    }, {
+        question: "Do you link your stylesheet in the html page?",
         answerOne: "yes",
         answerTwo: "no",
         answerThree: "sometimes",
@@ -43,57 +42,66 @@ var quizQuestionsEl = [
     }
 ]
 
-//moves from question to question
-button.addEventListener("click", () => {
-    setTime();
-question.style.display = "none";
-questionsEl.classList.remove("hide")
-displayQuestions()
-})
+var timerInterval;
 
-//displaying our quiz questions
-function displayQuestions () {
-showQuestionsEl.textContent = quizQuestionsEl[index].question
-button1El.textContent = quizQuestionsEl[index].answerOne
-button2El.textContent = quizQuestionsEl[index].answerTwo
-button3El.textContent = quizQuestionsEl[index].answerThree
-
-}
-//alerting user if question was correct or incorrect
-function nextQuestion () {
-  
-    if (quizQuestionsEl[index].correctAnswer===this.textContent){
-        alert("correct")
-    }
-    else{
-        alert("incorrect")
-        secondsLeft = secondsLeft - 10;
-    }
-index++
-displayQuestions();
-
-if (quizQuestionsEl[index]=== quizQuestionsEl[3]) {
-    clearInterval(timerInterval);
-    initialsEl.classList.remove("hide")
-       }
-
-}
-
-
-button1El.addEventListener ("click", nextQuestion)
-button2El.addEventListener ("click", nextQuestion)
-button3El.addEventListener ("click", nextQuestion)
 function setTime() {
-    var timerInterval = setInterval(function() {
+    timerInterval = setInterval(function () {
         secondsLeft--;
         timerEl.textContent = secondsLeft
 
-        if(secondsLeft === 0) {
+        if (secondsLeft === 0) {
             clearInterval(timerInterval);
             sendMessage();
         }
     }, 1000);
 }
+
+//moves from question to question
+button.addEventListener("click", () => {
+    setTime();
+    question.style.display = "none";
+    questionsEl.classList.remove("hide")
+    displayQuestions()
+})
+
+//displaying our quiz questions
+function displayQuestions() {
+    showQuestionsEl.textContent = quizQuestionsEl[index].question
+    button1El.textContent = quizQuestionsEl[index].answerOne
+    button2El.textContent = quizQuestionsEl[index].answerTwo
+    button3El.textContent = quizQuestionsEl[index].answerThree
+
+}
+//alerting user if question was correct or incorrect
+function nextQuestion() {
+
+    if (quizQuestionsEl[index].correctAnswer === this.textContent) {
+        alert("correct")
+    }
+    else {
+        alert("incorrect")
+        secondsLeft = secondsLeft - 10;
+    }
+    index++
+    if (quizQuestionsEl[index] === quizQuestionsEl[3]) {
+        clearInterval(timerInterval);
+        initialsEl.classList.remove("hide")
+    }
+
+    if (quizQuestionsEl[index] === quizQuestionsEl[3]) {
+        clearInterval(timerInterval);
+        initialsEl.classList.remove("hide")
+        questionsEl.classList.add("hide")
+        return;
+    }
+
+
+    displayQuestions();
+}
+
+button1El.addEventListener("click", nextQuestion)
+button2El.addEventListener("click", nextQuestion)
+button3El.addEventListener("click", nextQuestion)
 
 
 //letting the user know the time is up
